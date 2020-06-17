f43d316777789db2ebe7d7230f1a7cf920be5219
olman <olm@u.com>
2020-06-17 08:48:59 -0600

    ArreglosUnidimensionales

---
+++ IIParcial/frmArreglosVectores.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmArreglosVectores
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnGenerar = New System.Windows.Forms.Button()
+        Me.cmbComputadoras = New System.Windows.Forms.ComboBox()
+        Me.cmbPrecios = New System.Windows.Forms.ComboBox()
+        Me.btnGenerarPrecio = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCant = New System.Windows.Forms.TextBox()
+        Me.btnSolicitar = New System.Windows.Forms.Button()
+        Me.cmbCompus = New System.Windows.Forms.ComboBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnLimpiar)
+        Me.GroupBox1.Controls.Add(Me.cmbPrecios)
+        Me.GroupBox1.Controls.Add(Me.btnGenerarPrecio)
+        Me.GroupBox1.Controls.Add(Me.cmbComputadoras)
+        Me.GroupBox1.Controls.Add(Me.btnGenerar)
+        Me.GroupBox1.Location = New System.Drawing.Point(54, 39)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(375, 350)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Unidimensionales"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.cmbCompus)
+        Me.GroupBox2.Controls.Add(Me.btnSolicitar)
+        Me.GroupBox2.Controls.Add(Me.txtCant)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(484, 39)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(372, 346)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Dinaico"
+        '
+        'btnGenerar
+        '
+        Me.btnGenerar.Location = New System.Drawing.Point(24, 54)
+        Me.btnGenerar.Name = "btnGenerar"
+        Me.btnGenerar.Size = New System.Drawing.Size(92, 51)
+        Me.btnGenerar.TabIndex = 0
+        Me.btnGenerar.Text = "Generar computadoras"
+        Me.btnGenerar.UseVisualStyleBackColor = True
+        '
+        'cmbComputadoras
+        '
+        Me.cmbComputadoras.FormattingEnabled = True
+        Me.cmbComputadoras.Location = New System.Drawing.Point(12, 165)
+        Me.cmbComputadoras.Name = "cmbComputadoras"
+        Me.cmbComputadoras.Size = New System.Drawing.Size(121, 24)
+        Me.cmbComputadoras.TabIndex = 1
+        '
+        'cmbPrecios
+        '
+        Me.cmbPrecios.FormattingEnabled = True
+        Me.cmbPrecios.Location = New System.Drawing.Point(194, 165)
+        Me.cmbPrecios.Name = "cmbPrecios"
+        Me.cmbPrecios.Size = New System.Drawing.Size(121, 24)
+        Me.cmbPrecios.TabIndex = 3
+        '
+        'btnGenerarPrecio
+        '
+        Me.btnGenerarPrecio.Location = New System.Drawing.Point(206, 54)
+        Me.btnGenerarPrecio.Name = "btnGenerarPrecio"
+        Me.btnGenerarPrecio.Size = New System.Drawing.Size(92, 51)
+        Me.btnGenerarPrecio.TabIndex = 2
+        Me.btnGenerarPrecio.Text = "Generar Precios"
+        Me.btnGenerarPrecio.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Can. Computadoras"
+        '
+        'txtCant
+        '
+        Me.txtCant.Location = New System.Drawing.Point(157, 38)
+        Me.txtCant.Name = "txtCant"
+        Me.txtCant.Size = New System.Drawing.Size(100, 22)
+        Me.txtCant.TabIndex = 1
+        '
+        'btnSolicitar
+        '
+        Me.btnSolicitar.Location = New System.Drawing.Point(123, 82)
+        Me.btnSolicitar.Name = "btnSolicitar"
+        Me.btnSolicitar.Size = New System.Drawing.Size(75, 23)
+        Me.btnSolicitar.TabIndex = 2
+        Me.btnSolicitar.Text = "Solicitar"
+        Me.btnSolicitar.UseVisualStyleBackColor = True
+        '
+        'cmbCompus
+        '
+        Me.cmbCompus.FormattingEnabled = True
+        Me.cmbCompus.Location = New System.Drawing.Point(80, 165)
+        Me.cmbCompus.Name = "cmbCompus"
+        Me.cmbCompus.Size = New System.Drawing.Size(149, 24)
+        Me.cmbCompus.TabIndex = 3
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(115, 240)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 4
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'frmArreglosVectores
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1105, 500)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmArreglosVectores"
+        Me.Text = "frmArreglosVectores"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cmbComputadoras As ComboBox
+    Friend WithEvents btnGenerar As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents cmbPrecios As ComboBox
+    Friend WithEvents btnGenerarPrecio As Button
+    Friend WithEvents cmbCompus As ComboBox
+    Friend WithEvents btnSolicitar As Button
+    Friend WithEvents txtCant As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnLimpiar As Button
+End Class
